525650ffeb739ab467346d99bb52a52761f90912
toan0011 <nmtoan.0011@gmail.com>
2023-10-22 23:48:52 +0700

    Chua cai dat dieu huong

---
+++ CustomView2/DetailViewController.swift
@@ -0,0 +1,67 @@
+//
+//  DetailViewController.swift
+//  CustomView2
+//
+//  Created by Minh Toan on 22/10/2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var viewInsideBgChooseMale: UIView!
+    @IBOutlet weak var viewBgChooseFemale: UIView!
+    @IBOutlet weak var viewBgChooseMale: UIView!
+    @IBOutlet weak var viewContainFemale: UIView!
+    @IBOutlet weak var viewContainMale: UIView!
+    @IBOutlet weak var viewContainIncome: UIView!
+    @IBOutlet weak var viewContainJob: UIView!
+    @IBOutlet weak var viewContainBirth: UIView!
+    @IBOutlet weak var viewContainName: UIView!
+    @IBOutlet weak var viewInsideBgChooseFemale: UIView!
+    @IBOutlet weak var viewBgIconPhoto: UIView!
+    @IBOutlet weak var imgIconPhoto: UIImageView!
+    @IBOutlet weak var viewContainIconPhoto: UIView!
+    @IBOutlet weak var imgAvatar: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCornerRadius()
+        setBorderForView()
+        
+    }
+    
+    func setCornerRadius(){
+        imgAvatar.layer.cornerRadius = 20
+        viewContainName.layer.cornerRadius = 10
+        viewContainBirth.layer.cornerRadius = 10
+        viewContainJob.layer.cornerRadius = 10
+        viewContainIncome.layer.cornerRadius = 10
+        viewContainMale.layer.cornerRadius = 10
+        viewContainFemale.layer.cornerRadius = 10
+        viewContainIconPhoto.layer.cornerRadius = 10
+        viewBgIconPhoto.layer.cornerRadius = 8
+        viewBgChooseMale.layer.cornerRadius = viewBgChooseMale.bounds.width / 2
+        viewBgChooseFemale.layer.cornerRadius = viewBgChooseFemale.bounds.width / 2
+        viewInsideBgChooseMale.layer.cornerRadius = viewInsideBgChooseMale.bounds.width / 2
+        viewInsideBgChooseFemale.layer.cornerRadius = viewInsideBgChooseFemale.bounds.width / 2
+    }
+    
+    func setBorder(myview: UIView){
+        myview.layer.borderWidth = 1
+        myview.layer.borderColor = UIColor.lightGray.cgColor
+        myview.layer.cornerRadius = 10
+    }
+    
+    func setBorderForView(){
+        setBorder(myview: viewContainJob)
+        setBorder(myview: viewContainName)
+        setBorder(myview: viewContainBirth)
+        setBorder(myview: viewContainIncome)
+        setBorder(myview: viewContainMale)
+        setBorder(myview: viewContainFemale)
+        setBorder(myview: viewBgChooseMale)
+        setBorder(myview: viewBgChooseFemale)
+    }
+    
+
+}
--- CustomView2/ViewController.swift
+++ CustomView2/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewContainPersonalData: UIView!
+    @IBOutlet weak var viewBottom: UIView!
+    @IBOutlet weak var imgIconCommunity: UIImageView!
+    @IBOutlet weak var imgIconFaq: UIImageView!
+    @IBOutlet weak var imgIconHeart: UIImageView!
+    @IBOutlet weak var imgIconStatement: UIImageView!
+    @IBOutlet weak var imgIconSetting: UIImageView!
+    @IBOutlet weak var imgIconPerson: UIImageView!
+    @IBOutlet weak var imgAvatar: UIImageView!
+    @IBOutlet weak var imgIconHandbook: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setCornerRadiusForView()
+        
     }
+    
+    func setCornerRadiusForView(){
+        imgAvatar.layer.cornerRadius = 20
+        imgIconPerson.layer.cornerRadius = 10
+        imgIconSetting.layer.cornerRadius = 10
+        imgIconStatement.layer.cornerRadius = 10
+        imgIconHeart.layer.cornerRadius = 10
+        imgIconFaq.layer.cornerRadius = 10
+        imgIconHandbook.layer.cornerRadius = 10
+        imgIconCommunity.layer.cornerRadius = 10
+        viewBottom.layer.cornerRadius = 20
+    }
+    @IBAction func tapPersonalData(_ sender: Any) {
+        
+    }
+    
 
 
 }
